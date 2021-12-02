f5ee906778d0a816d3bc5863f07015cb3081149b
Kitsune-fx <kant.cmd@gmail.com>
2021-12-02 10:29:10 +0700

    [🥐 Cursed frontend]

--- client/app.js
+++ client/app.js
@@ -3,13 +3,22 @@ const path = require('path')
 
 const app = express()
 
-app.use('/static', express.static(path.join(__dirname, 'assets','static')))
+app.use('/static', express.static(path.resolve(__dirname, 'assets','static')))
 
 
-app.get("/*" , (req,res)=> {
+app.get("/" , (req,res)=> {
     // console.log(__dirname)
-    res.sendFile(path.resolve(__dirname, "assets", "index.html"));
+    res.sendFile(path.resolve(__dirname, "assets", "static","html","index.html"));
 } )
+
+app.get("/add" , (req,res)=> {
+    // console.log(__dirname)
+    res.sendFile(path.resolve(__dirname, "assets","static","html", "add.html"));
+} )
+
+app.get("*" , (req,res)=>{
+    res.sendFile(path.resolve(__dirname, "assets","static","html", "404.html"));
+})
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, ()=>console.log(`Server is running on Port: ${PORT}`));
